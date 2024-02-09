a85200bc1d9ce6ca96b0782d9b6a79b1e8806b49
Santosh Shah <santoshofficial679@gmail.com>
2024-02-09 12:49:59 +0545

    Entity of Comment and Post are implemented

---
+++ src/main/java/com/blogingsystem/blogingsystemapi/BlogingsystemapiApplication.java
@@ -0,0 +1,13 @@
+package com.blogingsystem.blogingsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogingsystemapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogingsystemapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogingsystem/blogingsystemapi/BlogingsystemapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogingsystem.blogingsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogingsystemapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
